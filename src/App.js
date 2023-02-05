672819b08a0a9b9da72d21bfab50f7725c1f499e
Robert Rothermel <thirdender@gmail.com>
2023-02-04 21:31:59 -0500

    Initial dial design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
+import Dial from './Dial';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.App}>
+      <header>
+        <h1>Greenhouse</h1>
       </header>
+      <main>
+        <div>
+          <Dial />
+          <h3>Temperature</h3>
+        </div>
+        <div>
+          <Dial />
+          <h3>Humidity</h3>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/Dial.js
@@ -0,0 +1,16 @@
+import styles from './Dial.module.scss';
+
+function Dial() {
+  return (
+    <div className={styles.Dial}>
+      <div>
+        <div>
+          80.4°
+        </div>
+      </div>
+      <span />
+    </div>
+  );
+}
+
+export default Dial;
